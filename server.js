def3ab4d95644ab6d71712a6c3a2304f38370f89
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-24 23:50:48 +0530

    dotenv setup done!

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const express = require('express');
-
+const express = require("express");
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 
-app.get('/', (request, response) => {
-    response.json({ message: 'Hello World!' });
-})
+
+app.get("/", (request, response) => {
+  response.json({ message: "Hello World!" });
+});
 
 app.listen(3001, () => {
-    console.log(`server is running @ http://localhost:3001`);
+  console.log(`server is running @ http://localhost:3001`);
 });
+
+mongoose.connect(process.env.MONGODB_URI)
+
+    .then(() => {
+        console.log('Connectin to Mongoose...');
+    })
+   .catch((error) => {
+       console.log('Error connection  to Mongoose', error);
+   })
+
