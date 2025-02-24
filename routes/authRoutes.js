7c57af8daef06a59bb6963c6dc26a21f029d8469
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-25 00:39:53 +0530

    setup router done!

---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const authRouter = express.Router();
+
+authRouter.get('/', (request, response) => {
+  response.json({ message: "Hello World!" });
+});
+
+
+module.exports = authRouter;
