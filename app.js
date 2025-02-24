060c7ec7a74a22587b4f415233745c22fa7ff120
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-25 01:58:22 +0530

    register endpoint --except bcrypt endpoint

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const authRouter = require("./routes/authRoutes");
 
 const app = express();
 
+app.use(express.json());
 app.use('/auth', authRouter);
 
 module.exports = app;
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,34 @@
+const User = require('../models/User');
+
 const authController = {
 
     register: async (request, response) => {
         try {
-            response.json({ message: "Hello World!" });
+            //get the details from the request body 
+            const { name, email, password } = request.body;
+
+            //validate the email
+            const user = await User.findOne({ email });
+
+            console.log(user);
+
+            // if the email is already in use , send a response
+            if (user) {
+                return response.status(400).json({ message: 'Email already in use' });
+            }
+
+            // Create a model object
+            const newUser = new User({ name, email, password });
+
+            // Save the user to the database 
+            await newUser.save();
+            
+            //send a response
+            response.status(201).json({ message: 'Registration successful' });
         } catch (error) {
             response.status(500).json({ message: error.message });
-     }
-
+        }
     }
+
 }
 module.exports = authController;
